d0a794c94f532fc495f3f9d62318f3dd68c1be2c
L.And <49897630+landre-cerp@users.noreply.github.com>
2026-06-14 13:27:00 +0200

    Feat(f18)/gear lights support (#224)
    * docs: update F-16C doc with AGP32 gear light support
    * Added Gearlight support
    
    ---------
    
    Co-authored-by: Cerppo <49897630+Cerppo@users.noreply.github.com>

--- Aircrafts/FA18C/FA18C_Listener.cs
+++ Aircrafts/FA18C/FA18C_Listener.cs
@@ -8,8 +8,14 @@ internal class FA18C_Listener : AircraftListener
 {
     private const string IFEI_PAGE = "IFEI";
 
-    private DCSBIOSOutput? MASTER_CAUTION_LT;
+    private DCSBIOSOutput? _MASTER_CAUTION_LT;
     private DCSBIOSOutput? _cockpitLightModeSw;
+
+    private DCSBIOSOutput? _FLP_LG_LEFT_GEAR_LT;
+    private DCSBIOSOutput? _FLP_LG_RIGHT_GEAR_LT;
+    private DCSBIOSOutput? _FLP_LG_NOSE_GEAR_LT;
+
+    private DCSBIOSOutput? _LANDING_GEAR_HANDLE_LT;
 
     uint _masterCaution = 0;
     uint _lightMode = 0; // 2=NVG, 1=NITE, 0=DAY
@@ -56,7 +62,7 @@ internal class FA18C_Listener : AircraftListener
         }
 
         Register(_cockpitLightModeSw, v => _lightMode = v);
-        Register(MASTER_CAUTION_LT, v =>
+        Register(_MASTER_CAUTION_LT, v =>
         {
             if (_masterCaution != v)
             {
@@ -68,12 +74,24 @@ internal class FA18C_Listener : AircraftListener
         _ufcPage.RegisterControls(RegisterString, () => _ufcPage.Render(GetCompositor(DEFAULT_PAGE)));
         _ifeiPage.RegisterControls(RegisterString, () => _ifeiPage.Render(GetCompositor(IFEI_PAGE), _lightMode));
     }
-    protected override void RegisterFrontpanelControls() { }
+    protected override void RegisterFrontpanelControls() { 
+
+        Register(_FLP_LG_LEFT_GEAR_LT, v => FlightDeck.GearLeftDown = v == 1);
+        Register(_FLP_LG_RIGHT_GEAR_LT, v => FlightDeck.GearRightDown = v == 1);
+        Register(_FLP_LG_NOSE_GEAR_LT, v => FlightDeck.GearNoseDown = v == 1);
+
+        Register(_LANDING_GEAR_HANDLE_LT, v => FlightDeck.GearWarning = v == 1);
+    }
 
     protected override void InitializeDcsBiosOutputs()
     {
-        MASTER_CAUTION_LT = DCSBIOSControlLocator.GetUIntDCSBIOSOutput("MASTER_CAUTION_LT");
+        _MASTER_CAUTION_LT = DCSBIOSControlLocator.GetUIntDCSBIOSOutput("MASTER_CAUTION_LT");
         _cockpitLightModeSw = DCSBIOSControlLocator.GetUIntDCSBIOSOutput("COCKKPIT_LIGHT_MODE_SW");
+
+        _FLP_LG_LEFT_GEAR_LT = DCSBIOSControlLocator.GetUIntDCSBIOSOutput("FLP_LG_LEFT_GEAR_LT");
+        _FLP_LG_RIGHT_GEAR_LT = DCSBIOSControlLocator.GetUIntDCSBIOSOutput("FLP_LG_RIGHT_GEAR_LT");
+        _FLP_LG_NOSE_GEAR_LT = DCSBIOSControlLocator.GetUIntDCSBIOSOutput("FLP_LG_NOSE_GEAR_LT");
+        _LANDING_GEAR_HANDLE_LT = DCSBIOSControlLocator.GetUIntDCSBIOSOutput("LANDING_GEAR_HANDLE_LT");
 
         _ufcPage.InitializeControls();
         _ifeiPage.InitializeControls();
